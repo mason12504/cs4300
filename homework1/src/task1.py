357e10aa4c3c4e2292eb1c74607e3460a52d1c2e
mason12504 <masonandersen3002@gmail.com>
2025-09-22 18:42:50 +0000

    add some files

---
+++ homework1/src/task1.py
@@ -0,0 +1,6 @@
+# python script
+
+def printTask1():
+    print("Hello World!")
+    return("Hello World!")
+
---
+++ homework1/tests/test_task1.py
@@ -0,0 +1,17 @@
+# Mason Andersen
+# CS 4300
+# Homework 1
+# 9-22-2025
+
+import sys
+import os
+
+# this is weird but it is the only method I tried from geeksforgeeks that works 
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current) 
+parent = parent + "/src"
+sys.path.append(parent)
+
+from task1 import *
+def test_task1():
+	assert printTask1() == "Hello World!" 
