98af40e56d493ead529f70da6af3e3906de7391c
mason12504 <masonandersen3002@gmail.com>
2025-09-22 22:03:56 +0000

    task4 done

--- homework1/src/task4.py
+++ homework1/src/task4.py
@@ -1,1 +1,18 @@
-# task4
+# task 4 Duck typing
+# Mason Andersen
+# CS 4300
+
+# calculates the final price with a discount
+# python will just know what type to use
+
+# need to accept integer or float for price and discount
+def calculate_discount(price, discount):
+    # assume if they type "10" for discount they mean 10% and not 1000% 
+    if discount > 1:
+        discount = discount/100
+    # otherwise just run it normally
+    finalPrice = price - (price * discount) 
+    return finalPrice
+
+
+
--- homework1/tests/test_task4.py
+++ homework1/tests/test_task4.py
@@ -10,4 +10,15 @@ parent = os.path.dirname(current)
 parent = parent + "/src"
 sys.path.append(parent)
 
-from task3 import * 
+from task4 import * 
+
+# just one big test
+def test_discount():
+    
+    # set up some values and edge cases like 0, 0.1, 1
+    priceList = [0, 1, 100, 49.1, 50.9999, 10000]
+    discountList = [100, 0.1, 0, -1, 50, 0.65] # negative discount behavior is basically a price increase
+    expectedResultList = [0, 0.9, 100, 98.2, 25.49995, 3500]
+
+    for price, discount, expectedResult in zip(priceList, discountList, expectedResultList):
+        assert(calculate_discount(price, discount) == expectedResult)
